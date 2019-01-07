5a848d1c45ddb867bc3729c504bc43ce77c23349
Peter Kerschbaumer <peter@kerschbaumer.se>
2019-01-07 17:56:07 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -28,6 +29,39 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  } else {
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+      res.status(200).send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  }
+});
+
+  // if (!ObjectID.isValid(id)) {
+  //   return console.log('ID is not valid!')
+  // }
+
+
+  // validate id using isValid
+    // 404 - send back empty body
+
+  // findById
+    // success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    // error
+      // 400 - send empty body
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
